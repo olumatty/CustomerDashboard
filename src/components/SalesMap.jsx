61a226a6c254cdf060d8ccd751b1392d68539be1
Matthew Adebayo <olumatty@gmail.com>
2025-02-09 00:49:20 +0100

    Setup 2.0

---
+++ src/components/SalesMap.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SalesMap = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SalesMap
---
+++ src/components/Volume.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Volume = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Volume
