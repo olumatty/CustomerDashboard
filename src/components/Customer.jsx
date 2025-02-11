791c65c5b2f958439383f1021f08b4533c40f01c
Matthew Adebayo <olumatty@gmail.com>
2025-02-11 11:20:35 +0100

    Dashboard 4.0

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import Graph from "../assets/Group 1000002786.png";
+import BlueTick from "../assets/BlueTick.svg";
+import GreenTick from "../assets/GreenTick.svg";
 
 const Customer = () => {
   return (
-    <div className='bg-white rounded-lg h-[260px] py-4 px-8'>
-      
-    </div>
-  )
-}
+    <div className="bg-white rounded-lg h-[260px] py-4 px-8">
+      <h2 className="font-semibold mb-4">Customer Satisfaction</h2>
+      <img src={Graph} alt="graph" />
+      <hr className="text-[#EDF2F6] mt-2" />
 
-export default Customer
+      <div className="flex gap-5 items-center justify-center mt-2">
+        <div className="flex gap-1">
+          <div className="mt-2">
+            <img src={BlueTick} alt="tick" />
+          </div>
+          <div className="flex flex-col">
+            <span className="text-[12px] text-[#96A5B8]">Last Month</span>
+            <span className="text-[11px]">$3,004</span>
+          </div>
+        </div>
+
+        {/* Vertical Line */}
+        <div className="w-[1px] h-8 bg-[#BDC9D3]"></div>
+
+        <div className="flex gap-1">
+          <div className="mt-2">
+            <img src={GreenTick} alt="tick" />
+          </div>
+          <div className="flex flex-col">
+            <span className="text-[12px] text-[#96A5B8]">This Month</span>
+            <span className="text-[11px]">$4,504</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Customer;
--- src/components/Revenue.jsx
+++ src/components/Revenue.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, CartesianGrid } from "recharts";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  CartesianGrid,
+} from "recharts";
 
 const data = [
   { day: "Monday", online: 13500, offline: 11500 },
@@ -13,16 +22,47 @@ const data = [
 
 const Revenue = () => {
   return (
-    <div className=' bg-white rounded-lg h-[260px] py-4 px-8 '>
-       <h2 className="font-semibold mb-4">Total Revenue</h2>
+    <div className=" bg-white rounded-lg h-[260px] py-4 px-8 ">
+      <h2 className="font-semibold mb-4">Total Revenue</h2>
       <ResponsiveContainer width="100%" height="80%">
         <BarChart data={data}>
-          <CartesianGrid vertical={false} horizontal={true} stroke="#EFF1F3" strokeDasharray="0" />
-          <XAxis dataKey="day" tick={{ fontSize: 10 }} interval={0} axisLine={false} tickLine={false} />
-          <YAxis tick={{ fontSize: 10 }} domain={[0, 25000]} ticks={[0, 5000, 10000, 15000, 20000, 25000]} axisLine={false} tickLine={false} tickFormatter={(value) => `${value / 1000}k`} />
+          <CartesianGrid
+            vertical={false}
+            horizontal={true}
+            stroke="#EFF1F3"
+            strokeDasharray="0"
+          />
+          <XAxis
+            dataKey="day"
+            tick={{ fontSize: 10 }}
+            interval={0}
+            axisLine={false}
+            tickLine={false}
+          />
+          <YAxis
+            tick={{ fontSize: 10 }}
+            domain={[0, 25000]}
+            ticks={[0, 5000, 10000, 15000, 20000, 25000]}
+            axisLine={false}
+            tickLine={false}
+            tickFormatter={(value) => `${value / 1000}k`}
+          />
           <Tooltip />
-          <Bar dataKey="online" fill="#0095FF" barSize={10} radius={[2, 2, 2, 2]} color="#7B91B0" />
-          <Bar dataKey="offline" fill="#00E096" name="Offline Sales" barSize={10} radius={[2, 2, 2, 2]} color="#7B91B0" />
+          <Bar
+            dataKey="online"
+            fill="#0095FF"
+            barSize={10}
+            radius={[2, 2, 2, 2]}
+            color="#7B91B0"
+          />
+          <Bar
+            dataKey="offline"
+            fill="#00E096"
+            name="Offline Sales"
+            barSize={10}
+            radius={[2, 2, 2, 2]}
+            color="#7B91B0"
+          />
         </BarChart>
       </ResponsiveContainer>
       <div className="flex gap-4 items-center justify-center">
@@ -37,7 +77,7 @@ const Revenue = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Revenue;
